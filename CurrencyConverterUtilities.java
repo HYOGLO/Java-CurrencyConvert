87809dfca1b18361f7d1b4b8dc90b7b5b442623b
Olaf Zaleski <58911455+HYOGLO@users.noreply.github.com>
2024-06-19 13:51:50 -0400

    Add files via upload

---
+++ CurrencyConverterUtilities.java
@@ -0,0 +1,7 @@
+public class CurrencyConverterUtilities {
+    
+    public static void main(String[] args) {
+        //{"English Pound", "Euro", "Japanese Yen", "South African Rand, "Canadian Dollar"}
+        //{0.79, 0.93, 149.31, 19.13, 1.35};
+    }
+}
